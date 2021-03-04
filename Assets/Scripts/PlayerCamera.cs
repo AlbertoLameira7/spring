074b76e5b94428105e074604d3e0efc240a77210
Alberto Lameira <lameira.alberto94@gmail.com>
2021-03-04 00:04:09 +0000

    Camera setup and player movement

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    [SerializeField] private GameObject _target;
+    [SerializeField] private float _cameraSmoothing = 5f;
+    Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - _target.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 targetCamPos = _target.transform.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, _cameraSmoothing * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private NavMeshAgent _nav;
+    private int _layerMask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _nav = gameObject.GetComponent<NavMeshAgent>();
+        _layerMask = 1 << 8;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, _layerMask))
+            {
+                Vector3 lookAtAngle = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+                transform.LookAt(lookAtAngle);
+                _nav.destination = hit.point;
+            }
+        }
+    }
+}
